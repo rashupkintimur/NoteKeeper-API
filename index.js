bc46c552c31228250ffa1542707f0076c9ed1b7e
xqdt <oxymususer@gmail.com>
2024-09-11 17:38:29 +0500

    added cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const db = require("./db");
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 
 // errors messages
